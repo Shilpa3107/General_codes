035014be2b472efea3a05ed4d192a4d7aa34052d
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-04-04 09:55:18 +0530

    Enclosed sc variable inside try block

--- Reach5Star.java
+++ Reach5Star.java
@@ -3,12 +3,14 @@ class Reach5Star
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
-		int y = sc.nextInt();
-		if((x+y) >= 2000)
-		System.out.println("Yes");
-		else 
-		System.out.println("No");
+        try (Scanner sc = new Scanner(System.in)) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if((x+y) >= 2000)
+            System.out.println("Yes");
+            else 
+            System.out.println("No");
+        }
+        
 	}
 }
