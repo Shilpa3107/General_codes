92baa6cc67c5dc734424d646324faaa40bb7f831
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-04-07 20:17:33 +0530

    Write a code of Minimum Operations to Exceed Threshold Value

---
+++ Threshold.java
@@ -0,0 +1,15 @@
+class Threshold {
+    public static void main(String args[]){
+        int nums[] = {2,11,10,1,3};
+        int k = 10;
+        System.out.println(minOperations(nums,k));
+    }
+    public static int minOperations(int[] nums, int k) {
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] < k)
+                count++;
+        }
+        return count;
+    }
+}
