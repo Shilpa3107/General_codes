09b0590d3bbcaad98a82334ed6efe54fe423ca50
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-04-08 07:34:02 +0530

    Write the code of Counting Number of Pairs With Absolute Difference K

---
+++ AbsoluteDiff.java
@@ -0,0 +1,19 @@
+class AbsoluteDiff {
+    public static void main(String args[]){
+    int nums[] = {1,2,2,1};
+    int k = 1;
+        System.out.println(countKDifference(nums,k));
+    }
+    public static int countKDifference(int[] nums, int k) {
+        int count = 0;
+        for(int i = 0;i<nums.length-1;i++){
+            for(int j = i+1;j<nums.length;j++){
+                if(i < j){
+                    if(Math.abs(nums[j]- nums[i]) == k)
+                        count++;
+                }
+            }
+        }
+        return count;
+    }
+}
