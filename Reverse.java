f38c7677b50390b2b1ad64150aa39b8230c40220
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-04-15 22:50:28 +0530

    Write a code of Reverse String

---
+++ Reverse.java
@@ -0,0 +1,20 @@
+class Reverse {
+    public static void main(String args[]){
+        char s[] = {'h','e','l','l','o'};
+        reverseString(s);
+        for(int i = 0;i<s.length;i++){
+            System.out.print(s[i]+" ");
+        }
+    }
+    public static void reverseString(char[] s) {
+        int i = 0,j =s.length-1;
+        while(i < j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+        
+    }
+}
