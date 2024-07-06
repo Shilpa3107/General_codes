09dde8dca9a1ae865319f74f11a6ddc053a17218
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-07-06 21:37:25 +0530

    Removed unnecessary packages

--- Previous_sum.java
+++ Previous_sum.java
@@ -1,13 +1,10 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 class Previous_sum {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        @SuppressWarnings("resource")
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
