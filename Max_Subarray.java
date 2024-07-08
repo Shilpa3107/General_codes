f4007b6a8e8534eea8695b4d8a1a87d21353a1f8
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-07-08 23:05:58 +0530

    Wrote the code of maximum sum of subarray using brute force technique

--- Max_Subarray.java
+++ Max_Subarray.java
@@ -1,19 +1,31 @@
+import java.util.ArrayList;
 class Max_Subarray{
     public static void main(String args[]){
         int arr[] = new int[5];
         for(int i= 0;i<5;i++){
             arr[i] = i+1;
         }
+        ArrayList<ArrayList<Integer>> listofLists = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
         int t = 3;
         int k = 0;
+        int sum = 0;
         for(int i = 0;i<=arr.length-t;i++){
+            ArrayList<Integer> list = new ArrayList<Integer>();
             while(k < t){
-                System.out.print(arr[i+k]+" ");
+                list.add(arr[i+k]);
+                sum = sum+arr[i+k];
                 k++;
             }
-            System.out.println();
+            list2.add(sum);
+            listofLists.add(list);
             k = 0;
-            
+            sum = 0;
         }
+        int max = Integer.MIN_VALUE;
+        for(int i : list2){
+            max = Math.max(max,i);
+        }
+        System.out.println(listofLists+" "+list2+" "+max);
     }
 }
