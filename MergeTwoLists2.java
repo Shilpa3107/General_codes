fbcb0c7cb8add8cf11d78330c421dc9714c5707e
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-09-05 23:39:37 +0530

    Wrote the code of merging two sorted lists

---
+++ MergeTwoLists2.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * c
+ */
+import java.util.ArrayList;
+class MergeTwoLists {
+    public static void main(String args[]){
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+       // list2.next.next = new ListNode(5);
+        ListNode curr = mergeTwoLists(list1,list2);
+        while(curr != null){
+            System.out.print(curr.val+" ");
+            curr = curr.next;
+        }
+    }
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+     ArrayList<Integer> arr = new ArrayList<Integer>();
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                arr.add(list1.val);
+                list1 = list1.next;
+            }
+            else{
+                arr.add(list2.val);
+                list2 = list2.next;
+            }
+        }
+        while(list1 != null){
+            arr.add(list1.val);
+            list1 = list1.next;
+        }
+        while(list2 != null){
+            arr.add(list2.val);
+            list2 = list2.next;
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        for(int i = 0;i<arr.size();i++){
+            curr.next = new ListNode(arr.get(i));
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
