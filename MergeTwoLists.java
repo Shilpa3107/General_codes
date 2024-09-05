61f712a4056155d609ced3f4650c8523de315411
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-09-06 00:00:16 +0530

    Added the listnode class

--- MergeTwoLists.java
+++ MergeTwoLists.java
@@ -1,15 +1,13 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 import java.util.ArrayList;
 import java.util.Collections;
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { 
+        this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
 class MergeTwoLists {
     public static void main(String args[]){
         ListNode head = new ListNode(1);
