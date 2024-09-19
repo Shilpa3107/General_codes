2f2ec60c7554b420e5cc4b6bc81db3927e631f6f
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-09-20 01:50:58 +0530

    Wrote the code of finding the maximum depth

---
+++ MaxDepth.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class MaxDepth {
+    public static void main(String args[]){
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(maxDepth(root));
+    }
+    public static int maxDepth(TreeNode root) {
+       return count(root);
+    }
+    public static int count(TreeNode root){
+        if(root == null)
+            return 0;
+        return Math.max(count(root.left),count(root.right))+1;
+    }
+}
