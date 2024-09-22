819ffb1db712aa56f94f1b9721cdc504ec76f7f8
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-09-23 01:33:40 +0530

    Wrote the code of finding the kth smallest element using arraylist and collections

---
+++ kthSmallest.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class kthSmallest{
+    public static void main(String args[]){
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(4);
+        root.left.right = new TreeNode(2);
+        int k = 1;
+        System.out.println(smallest(root,k));
+    }
+    public static int smallest(TreeNode root, int k){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        traversal(root,list);
+        return list.get(k-1);
+    }
+    public static void traversal(TreeNode root, ArrayList<Integer> list){
+        if(root == null)
+        return;
+        traversal(root.left,list);
+        list.add(root.val);
+        traversal(root.right,list);
+    }
+}
