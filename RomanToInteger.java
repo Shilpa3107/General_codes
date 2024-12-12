a233243ce671d67e613dfcd9ce8b93cf83eefdb9
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-12-12 12:26:27 +0530

    Wrote the code of converting roman to integers

---
+++ RomanToInteger.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+public class RomanToInteger{
+    public static void main(String args[]){
+      String s = "LVIII";
+      System.out.println(romanToInteger(s));
+    }
+    public static int romanToInteger(String s){
+        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int num = 0;
+        for(int i = 0;i<s.length();i++){
+            if(i < s.length()-1 && map.get(s.charAt(i)) < map.get(s.charAt(i+1)))
+              num -= map.get(s.charAt(i));
+            else 
+               num += map.get(s.charAt(i));
+        }
+        return num;
+    }
+}
