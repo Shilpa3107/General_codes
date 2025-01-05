280e0f2745b84142a70fc48ec7ae26e231e97ee1
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-01-06 00:03:12 +0530

    Wrote the code of excel column sheet

---
+++ ExcelColumnSheet.java
@@ -0,0 +1,15 @@
+public class ExcelColumnSheet{
+    public static void main(String args[]){
+        int columnNumber = 36;
+        System.out.println(excelColumnSheet(columnNumber));
+    }
+    public static String excelColumnSheet(int columnNumber){
+        StringBuilder sb = new StringBuilder();
+        while(columnNumber > 0){
+            columnNumber--;
+            sb.append((char)(columnNumber%26+'A'));
+            columnNumber /= 26;
+        }
+        return sb.reverse().toString();
+    }
+}
