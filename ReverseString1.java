c53b13a17b4910967703182d4eacdd5c66756885
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-02-02 22:10:02 +0530

    Changed the name of the file

--- ReverseString1.java
+++ ReverseString1.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-class ReverseString {
+class ReverseString1 {
     public static void main(String args[]){
         String s = "IceCream";
         System.out.println(reverseVowels(s));
