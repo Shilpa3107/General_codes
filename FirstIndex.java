abaa81f1e02cdc1aa4ec75efbd08a33e0f6c949e
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-02-06 14:25:25 +0530

    Wrote the code of finding the first index of the string

---
+++ FirstIndex.java
@@ -0,0 +1,10 @@
+public class FirstIndex{
+    public static void main(String args[]){
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        System.out.println(strStr(haystack, needle));
+    }
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
