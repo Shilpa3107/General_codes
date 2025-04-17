f5a2c449bfdf0b0ec0c60104f674c5e46d0fdf4b
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-04-17 21:49:31 +0530

    Wrote the code of finding the number of equal pairs based on condition

---
+++ CountEqualPairs.java
@@ -0,0 +1,18 @@
+public class CountEqualPairs{
+
+    public static void main(String args[]){
+        int nums[] = {3,1,2,2,2,1,3};
+        int k = 2;
+        System.out.println(countEqualPairs(nums,k));
+    }
+    public static int countEqualPairs(int nums[],int k){
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            for(int j = i+1;j<nums.length;j++){
+                if(nums[i]==nums[j] && (i*j)%k==0)
+                  count++;
+            }
+        }
+        return count;
+    }
+}
