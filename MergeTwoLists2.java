4e75726d96620dc4eb7f660bef15e84bf0e0f5d6
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-05-04 10:56:59 +0530

    Added listnode class

--- MergeTwoLists2.java
+++ MergeTwoLists2.java
@@ -46,3 +46,15 @@ class MergeTwoLists {
         return dummy.next;
     }
 }
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int val){
+        this.val = val;
+        next = null;
+    }
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
