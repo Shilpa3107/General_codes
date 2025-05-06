b208aaedd040da74c93ed77e15513c2630ad22e4
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-05-06 22:29:31 +0530

    Wrote the code to display coordinates

---
+++ Coordinates.py
@@ -0,0 +1,9 @@
+pos = {
+    "A":(2,3),
+    "B":(5,7),
+    "C":(0,0),
+    "D":(-1,4),
+    "E":(3,-2)
+}
+for node, point in pos.items():
+  print(f"Node : {node} Position: {point[0]},{point[1]}")
