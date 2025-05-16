56dee5d6d1251ed4f8fe813408174eafd844b864
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-05-16 07:32:16 +0530

    Wrote the code to get the words that has longest subsequence

---
+++ GetWordsInLongestSubsequence.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
+class GetWordsInLongestSubsequence {
+    public static void main(String args[]){
+        List<String> list = getWordsInLongestSubsequence(new String[]{"bab","dab","cab"},new int[]{1,2,2});
+        for(String s: list){
+            System.out.print(s+" ");
+        }
+    }
+    public static List<String> getWordsInLongestSubsequence(String[] words, int[] groups) {
+        int n = groups.length;
+        int[] dp = new int[n];
+        int[] prev = new int[n];
+        Arrays.fill(dp, 1);
+        Arrays.fill(prev, -1);
+        int maxIndex = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (
+                    check(words[i], words[j]) &&
+                    dp[j] + 1 > dp[i] &&
+                    groups[i] != groups[j]
+                ) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (dp[i] > dp[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        List<String> ans = new ArrayList<>();
+        for (int i = maxIndex; i >= 0; i = prev[i]) {
+            ans.add(words[i]);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+
+    private static boolean check(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int diff = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (++diff > 1) {
+                    return false;
+                }
+            }
+        }
+        return diff == 1;
+    }
+}
