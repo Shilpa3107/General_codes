2c5c9e1ae5ba2ecffdf2731e89f01e55aee207af
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-05-20 18:22:51 +0530

    Wrote the code to check if the array elements can become zero

---
+++ ZeroArray.java
@@ -0,0 +1,27 @@
+class ZeroArray {
+    public static void main(String args[]){
+        ZeroArray obj = new ZeroArray();
+        System.out.println(obj.isZeroArray(new int[]{1,0,1},new int[][]{{0,2}}));
+    }
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+          int[] deltaArray = new int[nums.length + 1];
+        for (int[] query : queries) {
+            int left = query[0];
+            int right = query[1];
+            deltaArray[left] += 1;
+            deltaArray[right + 1] -= 1;
+        }
+        int[] operationCounts = new int[deltaArray.length];
+        int currentOperations = 0;
+        for (int i = 0; i < deltaArray.length; i++) {
+            currentOperations += deltaArray[i];
+            operationCounts[i] = currentOperations;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (operationCounts[i] < nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
