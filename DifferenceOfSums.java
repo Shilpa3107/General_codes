fd23236f822dd5ee49cb39e899e95cd82d151a82
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-05-27 07:11:29 +0530

    Wrote the code to calculate the difference of sums

---
+++ DifferenceOfSums.java
@@ -0,0 +1,16 @@
+class DifferenceOfSums {
+    public static void main(String args[]){
+          DifferenceOfSums obj = new DifferenceOfSums();
+          System.out.println(obj.differenceOfSums(10,2));
+    }
+    public int differenceOfSums(int n, int m) {
+        int num1 = 0, num2 = 0;
+        for(int i = 1;i<=n;i++){
+            if(i%m==0)
+               num2 += i;
+            else 
+              num1 += i;
+        }
+        return num1-num2;
+    }
+}
