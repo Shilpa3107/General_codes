2e00b42fe12234b064d53edc72d4f30adf6f9866
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-06-02 00:37:56 +0530

    Wrote the code to solve the problem of distributing candies

---
+++ DistributeCandies.java
@@ -0,0 +1,17 @@
+class DistributeCandies {
+    public static void main(String args[]){
+        int n = 5, limit = 2;
+        DistributeCandies obj = new DistributeCandies();
+        System.out.println(obj.distributeCandies(n,limit));
+    }
+    public long distributeCandies(int n, int limit) {
+         long ans = 0;
+        for (int i = 0; i <= Math.min(limit, n); i++) {
+            if (n - i > 2 * limit) {
+                continue;
+            }
+            ans += Math.min(n - i, limit) - Math.max(0, n - i - limit) + 1;
+        }
+        return ans;
+    }
+}
