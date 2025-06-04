7a2f24e927ef0ee0b9b846ce0a0ecd6be199d2ea
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-06-04 08:43:52 +0530

    Wrote the code to split the words among friends in lexicographically largest

---
+++ AnswerString.java
@@ -0,0 +1,22 @@
+class AnswerString {
+    public static void main(String args[]){
+        String word = "dbca";
+        int numFriends = 2;
+        AnswerString obj = new AnswerString();
+        System.out.println(obj.answerString(word,numFriends));
+    }
+    public String answerString(String word, int numFriends) {
+        if (numFriends == 1) {
+            return word;
+        }
+        int n = word.length();
+        String res = "";
+        for (int i = 0; i < n; i++) {
+            String s = word.substring(i, Math.min(i + n - numFriends + 1, n));
+            if (res.compareTo(s) <= 0) {
+                res = s;
+            }
+        }
+        return res;
+    }
+}
