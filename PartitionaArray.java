f13f5f1f37b2493d18a0bd142d28a7f9ae7aa3d0
Shilpa3107 <shilpa..sinha3107@gmail.com>
2025-06-19 08:20:32 +0530

    Wrote the code to participation the array such that maximum difference is k

---
+++ PartitionaArray.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+public class PartitionaArray{
+    public static void main(String args[]){
+         int nums[] = {3,6,1,2,5};
+         System.out.println(partitionArray(nums,2));
+    }
+    public static int partitionArray(int nums[],int k){
+        Arrays.sort(nums);
+        int rec = nums[0];
+        int ans = 1;
+        for(int i = 1;i<nums.length;i++){
+            if(nums[i]-rec > k){
+                ans++;
+                rec = nums[i];
+            }
+        }
+        return ans;
+    }
+}
