5e07575490cc3551af8515bdd83d859271abeb47
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-06-26 23:54:58 +0530

    Wrote the code to find longest binary subsequnece less than k

---
+++ LongestSubsequence.java
@@ -0,0 +1,23 @@
+class LongestSubsequence {
+    public static void main(String args[]){
+        LongestSubsequence obj = new LongestSubsequence();
+        System.out.println(obj.longestSubsequence("1001010",5));
+    }
+    public int longestSubsequence(String s, int k) {
+         int sm = 0;
+        int cnt = 0;
+        int bits = (int) (Math.log(k) / Math.log(2)) + 1;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(s.length() - 1 - i);
+            if (ch == '1') {
+                if (i < bits && sm + (1 << i) <= k) {
+                    sm += 1 << i;
+                    cnt++;
+                }
+            } else {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
